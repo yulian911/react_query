04e3f18b4581a4501ffcc3fe230d50f4c85ecbfa
Yulian Melnychuk <julian95@o2.pl>
2023-01-19 15:06:35 +0100

    query react tutorial

---
+++ src/App.jsx
@@ -0,0 +1,50 @@
+import { useState } from 'react';
+import './App.css';
+import { useQuery, useMutation, useQueryClient } from '@tanstack/react-query';
+
+const POSTS = [
+  { id: 1, title: 'Post 1' },
+  { id: 2, title: 'Post 2' },
+  { id: 3, title: 'Post 3' },
+];
+
+const wait = time => {
+  return new Promise(resolve => setTimeout(resolve, time));
+};
+
+function App() {
+  const queryClient = useQueryClient();
+  const postQuery = useQuery({
+    queryKey: ['posts'],
+    queryFn: () => wait(1000).then(() => [...POSTS]),
+  });
+
+  const newPostMutation = useMutation({
+    mutationFn: title => {
+      return wait(1000).then(() => POSTS.push({ id: crypto.randomUUID(), title }));
+    },
+    onSuccess: () => {
+      queryClient.invalidateQueries('posts');
+    },
+  });
+
+  if (postQuery.isLoading) {
+    return <h1>Loading...</h1>;
+  }
+  if (postQuery.isError) {
+    return <pre>{JSON.stringify(postQuery.error)}</pre>;
+  }
+  console.log(postQuery.data);
+  return (
+    <div className="App">
+      {postQuery.data.map(el => (
+        <h1>{el.title}</h1>
+      ))}
+      <button disabled={postQuery.isLoading} onClick={() => newPostMutation.mutate('New Year')}>
+        Dodaj Post
+      </button>
+    </div>
+  );
+}
+
+export default App;

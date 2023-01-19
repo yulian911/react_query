596e1cf67dfd851aa86c47329e1d80ce86d80927
Yulian Melnychuk <julian95@o2.pl>
2023-01-19 15:24:51 +0100

    update

--- src/App.jsx
+++ src/App.jsx
@@ -11,40 +11,47 @@ const POSTS = [
 const wait = time => {
   return new Promise(resolve => setTimeout(resolve, time));
 };
+//  Podstawy examples
+// function App() {
+//   const queryClient = useQueryClient();
+//   const postQuery = useQuery({
+//     queryKey: ['posts'],
+//     queryFn: () => wait(1000).then(() => [...POSTS]),
+//   });
 
-function App() {
-  const queryClient = useQueryClient();
-  const postQuery = useQuery({
-    queryKey: ['posts'],
-    queryFn: () => wait(1000).then(() => [...POSTS]),
-  });
+//   const newPostMutation = useMutation({
+//     mutationFn: title => {
+//       return wait(1000).then(() => POSTS.push({ id: crypto.randomUUID(), title }));
+//     },
+//     onSuccess: () => {
+//       queryClient.invalidateQueries('posts');
+//     },
+//   });
 
-  const newPostMutation = useMutation({
-    mutationFn: title => {
-      return wait(1000).then(() => POSTS.push({ id: crypto.randomUUID(), title }));
-    },
-    onSuccess: () => {
-      queryClient.invalidateQueries('posts');
-    },
-  });
+//   if (postQuery.isLoading) {
+//     return <h1>Loading...</h1>;
+//   }
+//   if (postQuery.isError) {
+//     return <pre>{JSON.stringify(postQuery.error)}</pre>;
+//   }
+//   console.log(postQuery.data);
+//   return (
+//     <div className="App">
+//       {postQuery.data.map(el => (
+//         <h1>{el.title}</h1>
+//       ))}
+//       <button disabled={postQuery.isLoading} onClick={() => newPostMutation.mutate('New Year')}>
+//         Dodaj Post
+//       </button>
+//     </div>
+//   );
+// }
 
-  if (postQuery.isLoading) {
-    return <h1>Loading...</h1>;
-  }
-  if (postQuery.isError) {
-    return <pre>{JSON.stringify(postQuery.error)}</pre>;
-  }
-  console.log(postQuery.data);
-  return (
-    <div className="App">
-      {postQuery.data.map(el => (
-        <h1>{el.title}</h1>
-      ))}
-      <button disabled={postQuery.isLoading} onClick={() => newPostMutation.mutate('New Year')}>
-        Dodaj Post
-      </button>
-    </div>
-  );
-}
+// query keys !!
+// /posts -> ["posts"]
+// /posts/1 -> ["posts", post.id]
+// / posts?autorId=1 -> ["posts",{authorId:1}]
+// /posts/2/comments -> ["posts",post.id,"comments"]
 
+const App = () => {};
 export default App;
